0e875e2990f759defc6cabe73fc7d90b8533c09d
trailbehind-andrewljohnson <andrewljohnson@gmail.com>
2016-05-03 15:42:03 -0700

    normalize inputs to be 0->1 instead of 0->255, cifar didn't work with big numbers

--- src/label_chunks_cnn_cifar.py
+++ src/label_chunks_cnn_cifar.py
@@ -13,26 +13,34 @@ from tflearn.layers.core import input_data, dropout, fully_connected
 from tflearn.layers.conv import conv_2d, max_pool_2d
 from tflearn.layers.estimator import regression
 
+import numpy
+
 def train_neural_net(train_images, 
                      train_labels, 
                      test_images, 
                      test_labels):  
 
-	# Convolutional network building
-	network = input_data(shape=[None, 64, 64, 4])
-	network = conv_2d(network, 32, 3, activation='relu')
-	network = max_pool_2d(network, 2)
-	network = conv_2d(network, 64, 3, activation='relu')
-	network = conv_2d(network, 64, 3, activation='relu')
-	network = max_pool_2d(network, 2)
-	network = fully_connected(network, 512, activation='relu')
-	network = dropout(network, 0.5)
-	network = fully_connected(network, 2, activation='softmax')
-	network = regression(network, optimizer='adam',
-	                     loss='categorical_crossentropy',
-	                     learning_rate=0.001)
+  train_images = train_images.astype(numpy.float32)
+  train_images = numpy.multiply(train_images, 1.0 / 255.0)
+    
+  test_images = train_images.astype(numpy.float32)
+  test_images = numpy.multiply(train_images, 1.0 / 255.0)
 
-	# Train using classifier
-	model = tflearn.DNN(network, tensorboard_verbose=0)
-	model.fit(train_images, train_labels, n_epoch=50, shuffle=True, validation_set=(test_images, test_labels),
-	          show_metric=True, batch_size=96, run_id='cifar10_cnn')
+  # Convolutional network building
+  network = input_data(shape=[None, 64, 64, 4])
+  network = conv_2d(network, 32, 3, activation='relu')
+  #network = max_pool_2d(network, 2)
+  network = conv_2d(network, 64, 3, activation='relu')
+  network = conv_2d(network, 64, 3, activation='relu')
+  network = max_pool_2d(network, 2)
+  network = fully_connected(network, 512, activation='relu')
+  #network = dropout(network, 0.5)
+  network = fully_connected(network, 2, activation='softmax')
+  network = regression(network, optimizer='adam',
+                       loss='categorical_crossentropy',
+                       learning_rate=0.001)
+
+  # Train using classifier
+  model = tflearn.DNN(network, tensorboard_verbose=0)
+  model.fit(train_images, train_labels, n_epoch=50, shuffle=True, validation_set=(test_images, test_labels),
+            show_metric=True, batch_size=96, run_id='cifar10_cnn')
